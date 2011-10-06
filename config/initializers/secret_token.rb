8eb9a484eb9280a998a194fe3ddaf32ed821f22a
Kimball Bighorse <kbighorse@yahoo.com>
2011-10-05 17:06:17 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SteveJobsTribute::Application.config.secret_token = '4e1a9eaab399ffd98961d8e62499c46ec8aa8de697ef058753477f9fc49edf8a4e090079768d21598cb387ba0b585fae3e292167e9b44cf9cd958cfce8fe4230'
